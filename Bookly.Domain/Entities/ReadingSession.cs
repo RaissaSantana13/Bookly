00580bb4ca2d4ef68bc0583fd64465ef1706a3dc
Raissa Santana <raissasanta8@gmail.com>
2025-11-24 11:09:35 -0300

    ReadingSessionUpdate

---
+++ Bookly.Domain/Entities/ReadingSession.cs
@@ -0,0 +1,25 @@
+﻿using Bookly.Domain.Base;
+
+namespace Bookly.Domain.Entities
+{
+    public class ReadingSession : BaseEntity<int>
+    {
+        public ReadingSession()
+        {
+        }
+
+        public ReadingSession(int id, DateTime date, int pagesReadToday, User user, Book book, ReadingProcess readingProcess) : base(id)
+        {
+            Date=date;
+            PagesReadToday = pagesReadToday;
+            User = user;
+            Book = book;
+            ReadingProcess=readingProcess;
+        }
+        public DateTime Date { get; set; }
+        public int PagesReadToday { get; set; }
+        public User User { get; set; }
+        public Book Book { get; set; }
+        public ReadingProcess ReadingProcess { get; set; }
+    }
+}
